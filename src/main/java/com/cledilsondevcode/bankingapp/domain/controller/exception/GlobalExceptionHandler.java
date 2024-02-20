27485fdcfe6ab9153ff13eca1259b0a0ee812409
cledilson <cledilsondevoffice@gmail.com>
2024-02-20 15:37:02 -0300

    feat: Ajustes para primeira tentativa de deploy no Railway

---
+++ src/main/java/com/cledilsondevcode/bankingapp/domain/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.cledilsondevcode.bankingapp.domain.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+        return new ResponseEntity<>( "Resource ID nor found", HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+
+        String message = "Unexpected server error, see the logs.";
+        logger.error(message, unexpectedException);
+
+        return new ResponseEntity<>( message , HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
